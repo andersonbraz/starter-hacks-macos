eda4607c1edef06a35bcff3942434b375e8f887d
Anderson Braz <andersonbraz@users.noreply.github.com>
2020-09-02 08:23:56 -0300

    Update my-script.py

--- my-script.py
+++ my-script.py
@@ -1,3 +1,3 @@
 import os
 print("Step 1.0 - Install Homebrew")
-os.system("/bin/bash -c '$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)'")
+os.system('MYEXEC="(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"; /bin/bash -c "$MYEXEC"')
